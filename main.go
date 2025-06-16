f7205eabd338fe7ef85b78eda6bc1bd02836a22b
ihonwub <iheanyi.onwubiko@excella.com>
2025-06-16 14:00:49 -0400

    try: auto promotion Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("ihonwub is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("ihonwub is Awesome!!!!!", "larry3d", "green", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
